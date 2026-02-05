d8e44962de4f3608a0ab242e0d5c3ccea61c4372
Warrior_Bob <cwood31@my.centennialcollege.ca>
2026-02-05 15:48:25 -0500

    Lab 2 - Movement and Camera
    - added new 3-D Unity Project
    - camera is broken

---
+++ Lab-Assignments/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using KBCore.Refs;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputAction move;
+    private InputAction look;
+    private Vector3 velocity;
+    [SerializeField] private float maxSpeed = 10.0f;
+    [SerializeField] private float gravity = -5.0f;
+    [SerializeField] private float rotationSpeed = 4.0f;
+    [SerializeField] private float mouseSens = 5.0f;
+    [SerializeField, Self] private CharacterController controller;
+    [SerializeField, Child] private Camera cam;
+    private void OnValidate() {this.ValidateRefs();}
+    void Start()
+    {
+        move = InputSystem.actions.FindAction("Player/Move");
+        look = InputSystem.actions.FindAction("Player/Look");
+        // controller = GetComponent<CharacterController>();
+        // if (controller == null)
+            // controller = gameObject.AddComponent<CharacterController>();
+        //adds a CharacterController component if one does not already exist when the game starts
+        //not effective if you have multiple scripts trying to add the component at once
+        //not needed with Self attribute and ValidateRefs method
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 readMove = move.ReadValue<Vector2>();
+        Vector2 readLook = look.ReadValue<Vector2>();
+        Vector3 movement = transform.right * readMove.x
+        + transform.forward * readMove.y;
+
+        //controller.Move(movement * maxSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        //controller.Move(velocity * Time.deltaTime);
+        movement *= maxSpeed * Time.deltaTime;
+        controller.Move(movement);
+
+        transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
+        
+        mouseSens *= readLook.y;
+        mouseSens = Mathf.Clamp(mouseSens, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSens, 0, 0);
+    }
+}
